1ab8ef3044e68af171278a2b7d80d140d985ff47
CarlosJCD <carlos.calderon.d@icloud.com>
2024-02-12 18:35:33 -0600

    Video #18 - Sección #3

---
+++ 03-Strings/js/03-app.js
@@ -0,0 +1,11 @@
+const nombres = "Carlos Javier";
+const apellidos = "Calderon Delgado";
+
+console.log("Nombre Completo con concat: " + nombres.concat(` ${apellidos}`));
+
+console.log("Nombre Completo con operador +: " + nombres + " " + apellidos);
+
+console.log("Nombre Completo con comas: " , nombres , " " , apellidos);
+
+console.log(`Nombre completo con template strings: ${nombres} ${apellidos}`);
+
