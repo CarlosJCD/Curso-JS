ee9804820c56b3d246ba4d534f3d3e8e257549c4
CarlosJCD <carlos.calderon.d@icloud.com>
2024-02-12 18:52:45 -0600

    Video #22 - Sección #3 (FIN)

--- 03-Strings/js/06-app.js
+++ 03-Strings/js/06-app.js
@@ -2,4 +2,4 @@ const nombreCompleto = "Carlos Javier Calderon Delgado";
 
 console.log(`Nombre 3 veces: ${nombreCompleto.repeat(3)}`);
 
-console.log(`Arreglo del nombre: ${nombreCompleto.split(" ")}`)
+console.log(`Arreglo del nombre: ${nombreCompleto.split("  ")}`)
---
+++ 03-Strings/js/07-app.js
@@ -0,0 +1,5 @@
+const nombreCompleto = "Carlos Javier Calderon Delgado";
+
+
+console.log(`Nombre en mayúsculas: ${nombreCompleto.toLocaleUpperCase()}`);
+console.log(`Nombre en minúsculas: ${nombreCompleto.toLowerCase()}`);
