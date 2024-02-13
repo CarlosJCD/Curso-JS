73f353e72ea7d85ff2072a1d8c5be705ac838605
CarlosJCD <carlos.calderon.d@icloud.com>
2024-02-12 18:38:59 -0600

    Video #19 - Sección #3

---
+++ 03-Strings/js/04-app.js
@@ -0,0 +1,5 @@
+const nombreCompleto = "               Carlos Javier Calderon Delgado               ";
+
+console.log(`Nombre sin espacios al principio: ${nombreCompleto.trimStart()}`);
+console.log(`Nombre sin espacios al final: ${nombreCompleto.trimEnd()}`);
+console.log(`Nombre sin espacios: ${nombreCompleto.trim()}`);
