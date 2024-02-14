c5ebc61e6ff93d93eef3ed6f23406463dd85350a
CarlosJCD <carlos.calderon.d@icloud.com>
2024-02-13 23:05:39 -0600

    Video #28 - Sección #4 (FIN)

---
+++ 04-Numeros/js/06-app.js
@@ -0,0 +1,10 @@
+
+
+const numeroString = "20"
+
+const numeroInt = parseInt(numeroString)
+
+const numeroFloat = parseFloat(numeroString)
+
+console.log(typeof numeroInt);
+console.log(typeof numeroFloat);
