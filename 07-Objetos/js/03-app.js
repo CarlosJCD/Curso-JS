9fe5a006fe3e7330f051ff3f0561c19f7a7ef4ce
CarlosJCD <carlos.calderon.d@icloud.com>
2024-02-14 00:11:21 -0600

    Video #37 - Sección #7

---
+++ 07-Objetos/js/03-app.js
@@ -0,0 +1,15 @@
+const Persona = {
+    nombre: "Carlos Javier Calderon Delgado", 
+    edad: 20,
+    sexo: "masculino"
+ }
+
+ console.log(Persona);
+ 
+ Persona.últimosEstudios = "Bachillerato";
+ 
+ console.log(Persona);
+
+ delete Persona.últimosEstudios;
+
+ console.log(Persona);
