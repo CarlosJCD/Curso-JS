eb577a783e95a1f59261afd7a2e0759567f42b9f
CarlosJCD <carlos.calderon.d@icloud.com>
2024-02-15 07:57:29 -0600

    Video #50 - Sección #8

---
+++ 08-Arrays/js/03-app.js
@@ -0,0 +1,9 @@
+const meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto"];
+
+ console.table(meses);
+
+ console.log(`Meses registrados: ${meses.length}`);
+
+ for(let i = 0; i<meses.length< ++i;){
+    console.log("Mes regis trado" + meses[i]);
+ }
