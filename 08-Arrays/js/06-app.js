581e38bcff215eb02c32d3339ad7cba9eb46bb6f
CarlosJCD <carlos.calderon.d@icloud.com>
2024-02-15 07:57:55 -0600

    Video #53 - Sección #8

---
+++ 08-Arrays/js/06-app.js
@@ -0,0 +1,7 @@
+const meses = ["Enero","Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto"];
+
+const otrosMeses = ["Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+let todosLosMeses = [...meses, ...otrosMeses];
+
+console.log(todosLosMeses);
