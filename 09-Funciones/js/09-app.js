7f785fe6368b45cdb031a09bc597f0e6c41a40f8
CarlosJCD <carlos.calderon.d@icloud.com>
2024-02-15 09:08:36 -0600

    Video #66 - Sección #9

---
+++ 09-Funciones/js/09-app.js
@@ -0,0 +1,7 @@
+ const reproductor = {
+     reproducir: function(url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ") {
+        window.location.href = url
+     }  
+ }
+
+ reproductor.reproducir()
