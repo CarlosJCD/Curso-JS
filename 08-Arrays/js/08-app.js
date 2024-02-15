0f29c12e91a4d9e2d637c93e6cc36cd605a6fd07
CarlosJCD <carlos.calderon.d@icloud.com>
2024-02-15 07:58:07 -0600

    Video #55 - Sección #8

---
+++ 08-Arrays/js/08-app.js
@@ -0,0 +1,9 @@
+const meses = ["Enero","Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto"];
+
+
+const [primerMes, , , cuartoMes] = meses;
+
+
+console.log(primerMes);
+
+console.log(cuartoMes );
