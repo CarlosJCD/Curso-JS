8bebedd0333bcfa493e91829cbe907dad4eefe9d
CarlosJCD <carlos.calderon.d@icloud.com>
2024-02-17 22:15:02 -0600

    Video #102 - Sección #13

---
+++ 13-DOM/js/06-scripts.js
@@ -0,0 +1,8 @@
+
+const encabezado = document.querySelector(".contenido-hero h1");
+
+encabezado.innerText = "Hola";
+
+const imagen = document.querySelector(".card img");
+
+imagen.src = "img/hacer3.jpg"
