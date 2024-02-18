10a6ab3af48a45e564a04c975760c45b4d45f9a2
CarlosJCD <carlos.calderon.d@icloud.com>
2024-02-17 22:14:30 -0600

    Video #98 - Sección #13

---
+++ 13-DOM/js/02-scripts.js
@@ -0,0 +1,16 @@
+const header = document.getElementsByClassName("header");
+
+console.log(header);
+  
+const hero = document.getElementsByClassName("hero");
+
+console.log(hero);
+
+const contenedores = document.getElementsByClassName("contenedor");
+
+console.log(contenedores);
+
+const noExiste = document.getElementsByClassName("noExiste");
+
+console.log(noExiste);
+
