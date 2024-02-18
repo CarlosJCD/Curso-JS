0cdf0b1896848a3069156633f76df63270792f54
CarlosJCD <carlos.calderon.d@icloud.com>
2024-02-18 01:34:43 -0600

    Video #118 - Sección #15

--- 15-PROYECTO-Carrito/js/app.js
+++ 15-PROYECTO-Carrito/js/app.js
@@ -10,7 +10,20 @@ listaCursos.addEventListener("click", event => {
     event.preventDefault();
      
     if(event.target.classList.contains("agregar-carrito")){
-        console.log("agregando a carrito");
+        const cardCurso =  event.target.parentElement.parentElement;
+
+
+        const curso = {
+            id: cardCurso.querySelector("a").getAttribute("data-id"),
+            nombre: cardCurso.querySelector(".info-card h4").textContent,
+            precio: cardCurso.querySelector(".info-card p.precio span").textContent,
+            imagen: cardCurso.querySelector("img.imagen-curso").src,
+            cantidad: 1
+        }
+
+        console.log(curso);
+
+        
     }
 
 })
