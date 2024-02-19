c9aa9effeaf099d4bfc00e377e6d407b8361c12b
CarlosJCD <carlos.calderon.d@icloud.com>
2024-02-18 23:23:36 -0600

    Videos #127, #128 y #129 - Sección #16

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -6,4 +6,24 @@
  const botónEnviarFormulario = document.querySelectorAll("button")[0];
  const botónResetFormulario = document.querySelectorAll("button")[1];
 
- 
+inputEmail.addEventListener("blur", event => {
+    if(esVacío(event.target.value)) console.log("Email vacio");
+})
+
+inputAsunto.addEventListener("blur", event => {
+    if(esVacío(event.target.value)) console.log("Asunto vacio");
+})
+
+inputMensaje.addEventListener("blur", event => {
+    if(esVacío(event.target.value)) console.log("Mensaje vacio");
+})
+
+
+
+function validarEmail(email) {
+    
+}
+
+function esVacío(texto) {
+    return texto.trim() === "";
+}
