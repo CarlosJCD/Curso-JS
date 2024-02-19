87de53993baa582100f5af0fc435b5babc1ee9f7
CarlosJCD <carlos.calderon.d@icloud.com>
2024-02-18 23:07:24 -0600

    Video #126 - Sección #16 (INICIO)

---
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -0,0 +1,9 @@
+ 
+ const inputEmail = document.querySelector("#email");
+ const inputAsunto = document.querySelector("#asunto");
+ const inputMensaje = document.querySelector("#mensaje");
+
+ const botónEnviarFormulario = document.querySelectorAll("button")[0];
+ const botónResetFormulario = document.querySelectorAll("button")[1];
+
+ 
