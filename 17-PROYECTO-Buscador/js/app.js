c38c1ec45fdcda436156691dc265108e7db69cbd
CarlosJCD <carlos.calderon.d@icloud.com>
2024-02-19 23:06:48 -0600

    Video #145 - Sección #17

--- 17-PROYECTO-Buscador/js/app.js
+++ 17-PROYECTO-Buscador/js/app.js
@@ -1,8 +1,15 @@
 
 const contenedorResultadoBusqueda = document.getElementById("resultado");
 
+const selectAños = document.getElementById("year");
+
+const MAX_YEAR = new Date().getFullYear();
+const MIN_YEAR = MAX_YEAR - 10;
+
+
 document.addEventListener("DOMContentLoaded", event => {
     desplegarResultadoBúsqueda(autos)
+    cargarFiltroAños();
 })
 
 
@@ -18,4 +25,16 @@ function desplegarResultadoBúsqueda(autosFiltrados) {
         
         contenedorResultadoBusqueda.appendChild(registroAutoHTML);
     });
+}
+
+function cargarFiltroAños(){
+    for(let i = MAX_YEAR; i >= MIN_YEAR; i--){
+        const añoOption = document.createElement("option");
+        
+        añoOption.value = i;
+        añoOption.textContent = i;
+
+        selectAños.appendChild(añoOption)
+
+    }
 }
