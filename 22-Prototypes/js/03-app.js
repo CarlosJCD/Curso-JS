cb57cfd7b7aaa6df783e1642a12457d6f5d6ae6d
CarlosJCD <carlos.calderon.d@icloud.com>
2024-02-22 00:10:16 -0600

    Video #168 - Sección #22

---
+++ 22-Prototypes/js/03-app.js
@@ -0,0 +1,30 @@
+function Cliente(nombre, saldo) {
+    this.nombre = nombre;
+    this.saldo = saldo;
+}
+
+Cliente.prototype.tipoCliente = function () {
+    let tipo;
+
+    if(this.saldo > 10000){
+        tipo = "Gold";
+    } else if(this.saldo > 5000){
+        tipo = "Platinum"
+    } else {
+        tipo = "Normal"
+    }
+
+    return tipo;
+}
+
+Cliente.prototype.nombreClienteSlado = function() {
+    return `Nombre: ${this.nombre}, Saldo: ${this.saldo}, Tipo Cliente: ${this.tipoCliente()}`
+}
+
+const pedro = new Cliente("Pedro", 60000);
+
+console.log(pedro.tipoCliente());
+
+console.log(pedro.nombreClienteSlado());
+
+console.log(pedro);
