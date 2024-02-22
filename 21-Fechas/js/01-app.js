c16df1f6f95e35c58c29e48171b8a8e34e9f2fdb
CarlosJCD <carlos.calderon.d@icloud.com>
2024-02-21 23:20:22 -0600

    Video #164 - Sección #21 (INICIO)

---
+++ 21-Fechas/js/01-app.js
@@ -0,0 +1,24 @@
+const diaHoy = new Date();
+
+console.log(diaHoy);
+
+let valor = diaHoy.getFullYear();
+
+console.log(valor);
+
+valor = diaHoy.getMonth();
+
+console.log(valor);
+
+valor = diaHoy.getHours();
+
+console.log(valor);
+
+valor = diaHoy.getMinutes();
+
+console.log(valor);
+
+valor = diaHoy.getTime();
+
+console.log(valor);
+
