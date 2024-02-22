c7e73eb2661d78b529b08e78de5ffaf6eb197b56
CarlosJCD <carlos.calderon.d@icloud.com>
2024-02-22 09:42:26 -0600

    Video #172 - Sección #23

--- 23-PROYECTO-Prototypes-Seguro/js/app.js
+++ 23-PROYECTO-Prototypes-Seguro/js/app.js
@@ -1,22 +1,28 @@
 const MAX_AÑO = new Date().getFullYear()
 const MIN_AÑO = MAX_AÑO - 20;
 
-const selectAño = document.getElementById("year");
-
 function seguroAuto(marca, año, tipo) {
     this.marca = marca;
     this.año = año;
     this.tipo = tipo;
 }
 
-function VistaHTML() {}
+function VistaHTML() {
+    this.formulario = document.getElementById("cotizar-seguro");
+    this.selectMarca = document.getElementById("marca")
+    this.selectAño = document.getElementById("year");
+}
 
-VistaHTML.prototype.cargarSelectAños = () => {
+VistaHTML.prototype.obtenerInputRadioDelTipoDeSeguro = function() {
+    return document.querySelector("input[name='tipo']:checked")
+}
+
+VistaHTML.prototype.cargarSelectAños = function() {
      for(let añoActual = MAX_AÑO; añoActual >= MIN_AÑO; añoActual--){
         const optionAño = document.createElement("option")
         optionAño.textContent = añoActual;
 
-        selectAño.appendChild(optionAño);
+        this.selectAño.appendChild(optionAño);
     }
 }
 
@@ -24,5 +30,17 @@ let vista = new VistaHTML();
 
 document.addEventListener("DOMContentLoaded", () => {
     vista.cargarSelectAños();
+})
+
+vista.formulario.addEventListener("submit", evento =>{
+    evento.preventDefault();
+
+    const marca = vista.selectMarca.value;
+    const año = vista.selectAño.value;
+    const tipoDeSeguro = vista.obtenerInputRadioDelTipoDeSeguro().value;
+
+    if(!marca || !año || !tipoDeSeguro){
+        console.log("No pasa validación");
+    }
 })
 
