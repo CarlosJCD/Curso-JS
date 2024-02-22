fe1d758667d23c649e0f32e3c4b4f4697b27fecc
CarlosJCD <carlos.calderon.d@icloud.com>
2024-02-22 11:02:17 -0600

    Video #174 - Sección #23

--- 23-PROYECTO-Prototypes-Seguro/js/app.js
+++ 23-PROYECTO-Prototypes-Seguro/js/app.js
@@ -1,13 +1,45 @@
-const MAX_AÑO = new Date().getFullYear()
-const MIN_AÑO = MAX_AÑO - 20;
+const AÑO_ACTUAL = new Date().getFullYear()
+const AÑO_MINIMO = AÑO_ACTUAL - 20;
 
 const CLASES_CSS_ALERTA_ERROR = "mensaje mt-10 error";
-const CLASES_CSS_ALERTA_EXITO = "mensaje mt-10 correcto"
+const CLASES_CSS_ALERTA_ÉXITO = "mensaje mt-10 correcto"
 
-function SeguroAuto(marca, año, tipo) {
+const COTIZACIÓN_BASE = 2000;
+
+const ID_MARCA_AMERICANO = "1";
+const ID_MARCA_EUROPEO = "2";
+const ID_MARCA_ASIÁTICO = "3";
+
+
+const MODIFICADOR_MARCA_AMERICANO = 1.15;
+const MODIFICADOR_MARCA_EUROPEO =  1.05;
+const MODIFICADOR_MARCA_ASIÁTICO = 1.35;
+
+const SEGURO_BÁSICO = "basico";
+const SEGURO_COMPLETO = "completo";
+
+const MODIFICADOR_SEGURO_BÁSICO = 1.30;
+const MODIFICADOR_SEGURO_COMPLETO = 1.50;
+
+
+
+
+function SeguroAuto(marca, año, tipoDeSeguro) {
     this.marca = marca;
     this.año = año;
-    this.tipo = tipo;
+    this.tipoDeSeguro = tipoDeSeguro;
+}
+
+SeguroAuto.prototype.obtenerCotizaciónSeguro = function() {
+    const {marca, año, tipoDeSeguro} = this;
+
+    let cotización = cotizarPorMarca(marca);
+
+    cotización -= obtenerDescuentoPorAños(año, cotización);
+
+    cotización = aplicarCostoPorTipoDeSeguro(tipoDeSeguro, cotización);
+
+    return cotización;
 }
 
 
@@ -24,7 +56,7 @@ VistaHTML.prototype.obtenerInputRadioDelTipoDeSeguro = function() {
 }
 
 VistaHTML.prototype.cargarSelectAños = function() {
-     for(let añoActual = MAX_AÑO; añoActual >= MIN_AÑO; añoActual--){
+     for(let añoActual = AÑO_ACTUAL; añoActual >= AÑO_MINIMO; añoActual--){
         const optionAño = document.createElement("option")
         optionAño.textContent = añoActual;
 
@@ -36,7 +68,7 @@ VistaHTML.prototype.desplegarAlerta = function( mensajeDeAlerta, tipoDeAlerta = 
 
     const divMensajeDeAlerta = document.createElement("div");
 
-    tipoDeAlerta === "error" ? divMensajeDeAlerta.setAttribute("class",CLASES_CSS_ALERTA_ERROR) : divMensajeDeAlerta.setAttribute("class",CLASES_CSS_ALERTA_EXITO) 
+    tipoDeAlerta === "error" ? divMensajeDeAlerta.setAttribute("class",CLASES_CSS_ALERTA_ERROR) : divMensajeDeAlerta.setAttribute("class",CLASES_CSS_ALERTA_ÉXITO) 
 
     divMensajeDeAlerta.textContent = mensajeDeAlerta;
 
@@ -67,5 +99,53 @@ vista.formulario.addEventListener("submit", evento =>{
     }
 
     vista.desplegarAlerta("Cotizando...", "éxito");
+
+    let seguro = new SeguroAuto(marca, año, tipoDeSeguro);
+    
+    console.log(seguro.obtenerCotizaciónSeguro());
 })
 
+
+function cotizarPorMarca(marcaAuto) {
+    let cotización;
+
+    switch (marcaAuto) {
+        case ID_MARCA_AMERICANO:
+            cotización = COTIZACIÓN_BASE * MODIFICADOR_MARCA_AMERICANO;
+            break;
+        case ID_MARCA_EUROPEO:
+            cotización = COTIZACIÓN_BASE * MODIFICADOR_MARCA_EUROPEO;
+            break;
+
+        case ID_MARCA_ASIÁTICO:
+            cotización = COTIZACIÓN_BASE * MODIFICADOR_MARCA_EUROPEO;
+            break;
+        default:
+            cotización = COTIZACIÓN_BASE;
+            break;
+    }
+    return cotización;
+}
+
+function obtenerDescuentoPorAños(añoAuto, cotización){
+    const diferenciaEnAños =  (AÑO_ACTUAL - añoAuto) * 3 ;
+    const descuentoPorAño = ( diferenciaEnAños  * cotización ) / 100;
+
+    return descuentoPorAño;
+}
+
+function aplicarCostoPorTipoDeSeguro(tipoDeSeguro, cotización) {
+    let cotizaciónNueva;
+    switch (tipoDeSeguro) {
+        case SEGURO_BÁSICO:
+            cotizaciónNueva = cotización * MODIFICADOR_SEGURO_BÁSICO;
+            break;
+        case SEGURO_COMPLETO:
+            cotizaciónNueva = cotización * MODIFICADOR_SEGURO_COMPLETO;
+            break;
+        default:
+            cotizaciónNueva = cotización;
+            break;
+    }
+    return cotizaciónNueva;
+}
