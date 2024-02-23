5dcc70c7f59264c64630ffbe4db297126aa0274c
CarlosJCD <carlos.calderon.d@icloud.com>
2024-02-23 11:13:46 -0600

    Video #184 - Sección #25

--- 25-PROYECTO-GastoSemanal/js/app.js
+++ 25-PROYECTO-GastoSemanal/js/app.js
@@ -1,5 +1,17 @@
+
+const CLASES_CSS_ALERTA = "text-center alert";
+
+
+const CLASE_CSS_ALERTA_EXITO = "alert-success";
+const CLASE_CSS_ALERTA_ERROR = "alert-danger";
+
 class VistaHTML{
+    static divContenedorPrimario = document.querySelector(".primario");
+    
     static formAgregarGasto = document.getElementById("agregar-gasto");
+    static inputNombreGasto = document.getElementById("gasto");
+    static inputCantidadGasto = document.getElementById("cantidad");
+
     static ulGastoListado = document.querySelector("ul.list-group"); 
     static spanPresupuestoInicial = document.getElementById("total")
     static spanPresupuestoRestante = document.getElementById("restante")
@@ -7,6 +19,20 @@ class VistaHTML{
     static desplegarPresupuestoInicialYRestante(gastosSemanales){
         this.spanPresupuestoInicial.innerText = gastosSemanales.presupuestoInicial;    
         this.spanPresupuestoRestante.innerText = gastosSemanales.presupuestoRestante;    
+    }
+
+    static desplegarAlertaEnFormulario(mensaje, tipoDeAlerta = "error"){
+        const divAlerta = document.createElement("div");
+        divAlerta.textContent = mensaje;
+       
+        divAlerta.setAttribute("class", CLASES_CSS_ALERTA); 
+        tipoDeAlerta === "error" ? divAlerta.classList.add(CLASE_CSS_ALERTA_ERROR) : divAlerta.classList.add(CLASE_CSS_ALERTA_EXITO);
+
+        this.divContenedorPrimario.insertBefore(divAlerta, this.formAgregarGasto);
+
+        setTimeout(() => {
+            divAlerta.remove();
+        }, 3000);
     }
 }
 
@@ -23,6 +49,47 @@ class GastosSemanales{
     }
 }
 
+class Gasto{
+    #nombreGasto;
+    #cantidadGasto;
+
+    constructor(nombreGasto, cantidadGasto){
+        this.#nombreGasto = String(nombreGasto);
+        this.#cantidadGasto = Number(cantidadGasto);
+        this.alerta = {
+            tipoDeAlerta: '',
+            mensajeAlerta: ''
+        }
+    }
+
+    validarDatosDelGasto(){
+        
+        if(!this.#nombreGasto){
+            this.alerta.tipoDeAlerta = "error";
+            this.alerta.mensajeAlerta = "Por favor, ingrese un nombre de gasto";
+            return false;
+        }
+
+        if(!this.#cantidadGasto || this.#cantidadGasto <= 0 || isNaN(this.#cantidadGasto)){
+            this.alerta.tipoDeAlerta = "error";
+            this.alerta.mensajeAlerta = "Por favor, ingrese una cantidad valida ";
+            return false;
+        }
+
+        return true;
+    }
+
+    obtenerNombreDelGasto(){
+        return String(this.#nombreGasto);
+    }
+
+    obtenerCantidadDelGasto(){
+        return Number(this.#cantidadGasto);
+    }
+
+}
+
+
 const gastoSemanal = new GastosSemanales();
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -35,6 +102,25 @@ document.addEventListener("DOMContentLoaded", () => {
     
     
     VistaHTML.desplegarPresupuestoInicialYRestante(gastoSemanal);
+})
+
+VistaHTML.formAgregarGasto.addEventListener("submit", evento => {
+    evento.preventDefault()
+
+    const nombreGastoNuevo = VistaHTML.inputNombreGasto.value;
+    const cantidadGastoNuevo = VistaHTML.inputCantidadGasto.value;
+
+    const nuevoGasto = new Gasto(nombreGastoNuevo, cantidadGastoNuevo);
+
+    if(!nuevoGasto.validarDatosDelGasto()){
+        VistaHTML.desplegarAlertaEnFormulario(nuevoGasto.alerta.mensajeAlerta, nuevoGasto.alerta.tipoDeAlerta);
+    } else{
+        console.log("Desplegando gasto nuevo...");
+
+    }
+
+
+
 })
 
 
