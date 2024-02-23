5cdef809b9ddd87d63b2246646139b17c0f169a6
CarlosJCD <carlos.calderon.d@icloud.com>
2024-02-23 09:47:46 -0600

    Video #183 - Sección #25

--- 25-PROYECTO-GastoSemanal/js/app.js
+++ 25-PROYECTO-GastoSemanal/js/app.js
@@ -1,29 +1,40 @@
 class VistaHTML{
-    static formulario = document.getElementById("agregar-gasto");
-    static gastoListado = document.getElementsByClassName("list-group")[0];
+    static formAgregarGasto = document.getElementById("agregar-gasto");
+    static ulGastoListado = document.querySelector("ul.list-group"); 
+    static spanPresupuestoInicial = document.getElementById("total")
+    static spanPresupuestoRestante = document.getElementById("restante")
+
+    static desplegarPresupuestoInicialYRestante(gastosSemanales){
+        this.spanPresupuestoInicial.innerText = gastosSemanales.presupuestoInicial;    
+        this.spanPresupuestoRestante.innerText = gastosSemanales.presupuestoRestante;    
+    }
 }
 
 class GastosSemanales{
-    constructor(presupuesto = 0){
-        this.presupuesto = Number(presupuesto);
-        this.restante = Number(presupuesto);
+    constructor(presupuestoInicial = 0){
+        this.presupuestoInicial = Number(presupuestoInicial);
+        this.presupuestoRestante = Number(presupuestoInicial);
         this.gastos = [];
     }
-
-    setPresupuesto(presupuesto){
-        this.presupuesto = Number(presupuesto);
+    
+    setPresupuesto(presupuestoInicial){
+        this.presupuestoInicial = Number(presupuestoInicial);
+        this.presupuestoRestante = Number(presupuestoInicial);
     }
 }
 
 const gastoSemanal = new GastosSemanales();
 
 document.addEventListener("DOMContentLoaded", () => {
-    const presupuesto = prompt("Cual es tu presupuesto de esta semana?")
+    const presupuesto = prompt("Cual es tu presupuesto de esta semana?");
 
     if(!presupuestoValido(presupuesto)) window.location.reload();
 
 
     gastoSemanal.setPresupuesto(presupuesto);
+    
+    
+    VistaHTML.desplegarPresupuestoInicialYRestante(gastoSemanal);
 })
 
 
