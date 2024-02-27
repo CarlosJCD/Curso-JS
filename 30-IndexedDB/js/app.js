33facaf80720ba4f266b4d884e125cd8722d513b
CarlosJCD <carlos.calderon.d@icloud.com>
2024-02-27 08:30:08 -0600

    Video #216 - Sección #30 (FIN)

--- 30-IndexedDB/js/app.js
+++ 30-IndexedDB/js/app.js
@@ -1,3 +1,5 @@
+let DB;
+
 document.addEventListener("DOMContentLoaded", ()=>{
     let crmDB = window.indexedDB.open('crm',1.0)
 
@@ -7,6 +9,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
     crmDB.onsuccess = function() {
         console.log("Base de datos creada");
+        DB = crmDB.result;
     }
 
     crmDB.onupgradeneeded = function(e){
@@ -20,6 +23,34 @@ document.addEventListener("DOMContentLoaded", ()=>{
         objectStore.createIndex("nombre","nombre",{unique: false})
         objectStore.createIndex("email","email",{unique: true})
         objectStore.createIndex("telefono","telefono",{unique: false})
-        
     }
+
+    setTimeout(()=>{
+        crearCliente();
+    },5000)
 })
+
+function crearCliente() {
+    let transaction = DB.transaction(['crm'],'readwrite')
+
+    transaction.oncomplete = function(){
+        console.log('Transacción Completada');
+    }
+    
+    transaction.onerror = function(){
+        console.log('Hubo un error');
+    }
+
+    const objectStore = transaction.objectStore('crm');
+
+    const nuevoCliente = {
+        telefono: 9999038088,
+        nombre: "Carlos",
+        email: "email@email.com"
+    }
+
+    const peticion = objectStore.add(nuevoCliente);
+
+    console.log(peticion);
+
+}
