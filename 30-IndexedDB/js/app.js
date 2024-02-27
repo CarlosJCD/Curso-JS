dfd7f03a889031c7782ffbf188a3d8d01837bc6f
CarlosJCD <carlos.calderon.d@icloud.com>
2024-02-27 08:17:51 -0600

    Video #215 - Sección #30

--- 30-IndexedDB/js/app.js
+++ 30-IndexedDB/js/app.js
@@ -9,7 +9,17 @@ document.addEventListener("DOMContentLoaded", ()=>{
         console.log("Base de datos creada");
     }
 
-    crmDB.onupgradeneeded = function(){
-        console.log("Upgrade needed");
+    crmDB.onupgradeneeded = function(e){
+        const db = e.target.result;
+
+        const objectStore = db.createObjectStore('crm',{
+            keyPath: 'crm',
+            autoIncrement: true
+        });
+
+        objectStore.createIndex("nombre","nombre",{unique: false})
+        objectStore.createIndex("email","email",{unique: true})
+        objectStore.createIndex("telefono","telefono",{unique: false})
+        
     }
 })
